43911965de2c93385e08320ddb9e9298ec8573e2
Lorena <lorenaborgessilva@gmail.com>
2023-10-14 00:30:32 -0300

    atualização-html

--- build/exercicio1a.js
+++ build/exercicio1a.js
@@ -1,8 +1,10 @@
 "use strict";
 //01. Criar uma função que retorne a quantidade de vogais da palavra passada.
 //A) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
-function quantidadeVogal(palavra, vogais, quantidade, i) {
-    let letras = palavra.toUpperCase().split('');
+function quantidadeVogal(palavra) {
+    const vogais = ['A', 'E', 'I', 'O', 'U'];
+    let quantidade = 0;
+    const letras = palavra.toUpperCase().split('');
     for (let i = 0; i < letras.length; i++) {
         // console.log(letras[i])
         if (vogais.includes(letras[i])) {
@@ -12,4 +14,4 @@ function quantidadeVogal(palavra, vogais, quantidade, i) {
     console.log(`***A quantidade de vogais da palavra "${palavra}" é: `);
     return quantidade;
 }
-console.log(quantidadeVogal('hello', ['A', 'E', 'I', 'O', 'U'], 0, 0));
+console.log(quantidadeVogal('hello'));
